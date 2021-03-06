71daa345a2734b6a473c190d61b51e865e62525e
Toddy Mladenov <toddysm@gmail.com>
2021-03-06 12:05:50 -0800

    Added regex for matching distribution reference

--- scripts/registry-resolution/resolve_registry_endpoint.py
+++ scripts/registry-resolution/resolve_registry_endpoint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-:mod: `resolve_registry_endpoint.py` -- Resolves and analyses cxontainer registry's DNS name
+:mod: `resolve_registry_endpoint.py` -- Resolves and analyses container registry's DNS name
 ========================================================================================
 
     module:: resolve_registry_endpoint
@@ -12,19 +12,27 @@
 import argparse
 import logging
 import os
+import re
 import sys
 
 from random import choices
 from colorama import init, Fore, Back
 
 # Configure logging
-log_msg_format = "%(asctime)s (%(module)s --> %(funcName)s[%(lineno)d]) [%(levelname)s]: %(message)s"
-log_level = logging.DEBUG
-logging.basicConfig(filename="registry-resolution.log", filemode='w', format=log_msg_format, level=log_level)
+_log_msg_format = "%(asctime)s [%(levelname)s]: %(message)s"
+_log_level = logging.DEBUG
+logging.basicConfig(handlers=[logging.FileHandler("registry-resolution.log"),
+                              logging.StreamHandler()], format=_log_msg_format, level=_log_level)
+
+# RegEx for matching the docker distribution reference
+domain_regex = r"(?:[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]){1,63}(?:[.](?:[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]){1,63})+"
+repo_regex = r"/(?:[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])(?:/[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])*"
+tag_regex = r"[\w][\w.-]{0,127}"
+sha_regex = r"[A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][[:xdigit:]]{32,}"
 
 if __name__ == '__main__':
     """Resolves the container registry's endpoint DNS name and provides additional
-    details about the DNS resolution.
+    details about the DNS.
 
     It prints the information on the console and logs it to a `registry-resolution.log`
     file saved locally.
@@ -34,9 +42,14 @@ if __name__ == '__main__':
 
     # parse the command line arguments
     parser = argparse.ArgumentParser(description="Resolves and analyses the container registry's DNS name")
-    parser.add_argument('-i', '--image', help="The image for which to analyze the container registry name. \
-                        Must be in the format `[registry-name].[domain]/[repository]:[tag-or-sha]", required=True)
+    parser.add_argument('-i', '--image', help="The image for which to analyze the container registry name.\n \
+                        Must be in one of the following formats:\n \
+                         - `[registry-name].[domain]:[port]/[repository]:[tag-or-sha]\n \
+                         - `[registry-name].[domain]/[repository]:[tag-or-sha]`\n \
+                         - `[registry-name].[domain]/[repository]` (assumes `latest`)\n \
+                         - `[repository]:[tag-or-sha]`\n (assumes DockerHub endpoint `registry-`1.docker.io`) \
+                         - `[repository]` (assumes DockerHub endpoint `registry-`1.docker.io` and `latest`)\n", required=True)
                         
     args = parser.parse_args()
 
-    logging.info("Analyzing the container registry DNS for image: " + Fore.GREEN + f"{args.image}")
+    logging.info(f"Analyzing the container registry DNS for image: {args.image}")
